9718bc47adee224b8b62945b5a46eacf1e2f2018
Justin Kian Romeon <justin.romeon@tontine.com>
2025-11-16 15:27:02 +0800

    docs: add Demo.hs with example Project instances

---
+++ src/Demo.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Demo where
+
+import Project
+
+someProject :: Project
+someProject = GroupProject "Central Luzon" [tarlac, pampanga, nuevaEcija]
+  where
+    tarlac     = SingleProject 1 "Tarlac"
+    pampanga   = SingleProject 2 "Pampanga"
+    nuevaEcija = GroupProject "Nueva Ecija" [cabanatuan, gapan]
+    cabanatuan = SingleProject 3 "Cabanatuan City"
+    gapan      = SingleProject 4 "Gapan City"
